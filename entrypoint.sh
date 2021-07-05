0e495d6a42c63e9eff725a09fada1dedcc868291
Gabriel Ferreira <gabriel.oliveira@platformbuilders.io>
2021-07-05 17:15:50 -0300

    release candidate and release conditionals

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,14 @@ if [[ "$REF" == "develop" ]]; then
     echo "RELEASE_VERSION=$COMMIT_ID" >> $GITHUB_ENV
     echo "GITOPS_BRANCH=develop" >> $GITHUB_ENV
 
-elif [[ "$REF" == *"tags"* ]]; then
+elif [[ "$REF" == *"-rc_"* ]]; then
+    printf "New tag: ${GITHUB_REF#refs/*/}"
+    echo "RELEASE_VERSION=${GITHUB_REF#refs/*/}" >> $GITHUB_ENV
+    echo "GITOPS_BRANCH=homolog" >> $GITHUB_ENV
+
+elif [[ ${GITHUB_REF#refs/*/} =~ ^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$ ]]; then
     printf "New tag: ${GITHUB_REF#refs/*/}"
     echo "RELEASE_VERSION=${GITHUB_REF#refs/*/}" >> $GITHUB_ENV
     echo "GITOPS_BRANCH=release" >> $GITHUB_ENV
+
 fi
