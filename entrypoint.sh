492bf885c128231e76c0b62e01e15827b12ca3da
Rafael Karczevski B <146132286+rafael-karczevski-builder@users.noreply.github.com>
2024-01-22 14:06:45 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,11 @@ elif [[ "$REF" == *"-rc_"* ]]; then
     echo "RELEASE_VERSION=${GITHUB_REF#refs/*/}" >> $GITHUB_ENV
     echo "GITOPS_BRANCH=homolog" >> $GITHUB_ENV
 
+elif [[ "$REF" == "homolog" ]]; then
+    printf "Branch name: $REF \nCommit ID: $COMMIT_ID"
+    echo "RELEASE_VERSION=$COMMIT_ID" >> $GITHUB_ENV
+    echo "GITOPS_BRANCH=homolog" >> $GITHUB_ENV
+
 elif [[ ${GITHUB_REF#refs/*/} =~ ^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$ ]]; then
     printf "New tag: ${GITHUB_REF#refs/*/}"
     echo "RELEASE_VERSION=${GITHUB_REF#refs/*/}" >> $GITHUB_ENV
